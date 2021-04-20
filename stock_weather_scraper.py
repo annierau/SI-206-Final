b1524f7e2716027d10030ef8ca1eb7a901452fea
tejseth <55564043+tejseth@users.noreply.github.com>
2021-04-20 08:40:18 -0400

    Add files via upload

--- stock_weather_scraper.py
+++ stock_weather_scraper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[135]:
 
 
 from bs4 import BeautifulSoup
@@ -12,7 +12,7 @@ import numpy as np
 import seaborn as sns
 
 
-# In[55]:
+# In[136]:
 
 
 def get_soup(url):
@@ -24,7 +24,7 @@ def get_soup(url):
     return soup
 
 
-# In[56]:
+# In[137]:
 
 
 def get_filtered_dict(tags):
@@ -47,7 +47,7 @@ def get_filtered_dict(tags):
     return filtered_dict
 
 
-# In[57]:
+# In[138]:
 
 
 soup_1 = get_soup("https://www.forbes.com/advisor/investing/weather-effect-stock-market/")
@@ -55,7 +55,7 @@ tags1 = soup_1.find('div', class_ = "article-body inarticle-link-tracking fs-art
 filtered_dict1 = get_filtered_dict(tags1)
 
 
-# In[59]:
+# In[139]:
 
 
 plt.figure(figsize=(10,10))
@@ -67,13 +67,13 @@ my_range_1=range(1,len(ordered_df_1.index)+1)
 plt.hlines(y=my_range_1, xmin=0, xmax=ordered_df_1['count'], color='blue')
 plt.plot(ordered_df_1['count'], my_range_1, "o", color = "black")
 plt.yticks(my_range_1, ordered_df_1['word'])
-plt.title("The Top 25 Words Used by the Forbes to \n Describe Weather and Stocks", loc='center', fontweight='bold')
-plt.xlabel('Word Count')
-plt.ylabel('Word')
+plt.title("The Top 25 Words Used by the Forbes to \n Describe Weather and Stocks", loc='center', fontweight='bold', size = 30)
+plt.xlabel('Word Count', fontweight = "bold", size = 18)
+plt.ylabel('Word', fontweight = "bold", size = 18)
 plt.show()
 
 
-# In[60]:
+# In[140]:
 
 
 soup_2 = get_soup("https://www.investopedia.com/articles/markets/111015/does-weather-affect-stock-market.asp")
@@ -81,7 +81,7 @@ tags2 = soup_2.find('div', class_ = "comp article-body-content mntl-sc-page mntl
 filtered_dict2 = get_filtered_dict(tags2)
 
 
-# In[61]:
+# In[141]:
 
 
 plt.figure(figsize=(10,10))
@@ -94,12 +94,13 @@ y_pos = np.arange(len(bars))
 plt.barh(y_pos, height, color=(0.1, 0.1, 0.1, 0.1),  edgecolor='blue')
 plt.yticks(y_pos, bars)
 plt.title("The Top 25 Words Used by the 'Investopedia' \nto Describe Stocks and Weather", loc='center', fontweight='bold')
-plt.ylabel('Word Count')
-plt.ylabel('Word')
+plt.xlabel('Word Count', fontweight = "bold", size = 18)
+plt.tick_params(axis='both', labelsize = 14)
+plt.ylabel('Word', fontweight = "bold", size = 18)
 plt.show()
 
 
-# In[62]:
+# In[142]:
 
 
 soup_3 = get_soup("https://www.marketwatch.com/story/stock-market-weather-report-has-something-for-bulls-and-bears-2016-09-30")
@@ -107,7 +108,7 @@ tags3 = soup_3.find('div', class_ = "article__body article-wrap at16-col16 barro
 filtered_dict3 = get_filtered_dict(tags3)
 
 
-# In[63]:
+# In[143]:
 
 
 df_3 = pd.DataFrame(list(filtered_dict3.items()),columns = ['word','count']) 
@@ -115,7 +116,7 @@ df_top25_3 = df_3[0:25]
 #print(df_top25_3)
 
 
-# In[64]:
+# In[144]:
 
 
 plt.figure(figsize=(10,10))
@@ -124,12 +125,12 @@ values=[11, 8, 6, 3]
 explode = (0.0, 0.05, 0.0, 0)  
 colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99']
 plt.pie(values, labels=names, labeldistance=1.15, explode = explode, autopct='%1.1f%%',
-        shadow=True, startangle=90, colors = colors);
-plt.title("Investors React More to When It's Sunny than Cloudy (According to MarketWatch)", loc='center', fontweight='bold')
+        shadow=True, startangle=90, colors = colors, textprops={'fontsize': 16});
+plt.title("Investors React More to When It's Sunny\n than Cloudy (According to MarketWatch)", loc='center', fontweight='bold', size = 30)
 plt.show()
 
 
-# In[65]:
+# In[145]:
 
 
 def get_all_text(tags_list):
@@ -158,7 +159,7 @@ for key, value in all_sorted.items():
 #print(all_filtered)
 
 
-# In[66]:
+# In[146]:
 
 
 all_df = pd.DataFrame(list(all_filtered.items()),columns = ['word','count']) 
@@ -166,7 +167,7 @@ all_top_25 = all_df[0:25]
 #print(all_top_25)
 
 
-# In[67]:
+# In[147]:
 
 
 plt.figure(figsize=(10,10))
@@ -181,7 +182,7 @@ width = 2*np.pi / len(all_top_25['word'])
 indexes = list(range(1, len(all_top_25.index)+1))
 angles = [element * width for element in indexes]
 
-plt.title('Most Common Stock Market Weather Words Used in All Three Articles', loc = 'center', fontweight='bold')
+plt.title('Most Common Stock Market Weather \nWords Used in All Three Articles', loc = 'center', fontweight='bold', size = 30)
 
 bars = ax.bar(
     x=angles, 
@@ -211,10 +212,11 @@ for bar, angle, height, label in zip(bars,angles, heights, all_top_25["word"]):
         ha=alignment, 
         va='center', 
         rotation=rotation, 
-        rotation_mode="anchor") 
+        rotation_mode="anchor",
+        size = 15) 
 
 
-# In[107]:
+# In[148]:
 
 
 url = f'https://docs.google.com/spreadsheets/d/e/2PACX-1vSpI-1mMu_veN7L4W3STjh1QFzXf_13jH6vm4jd9hzolr-pq8fxV0VfBYftNycRkQ_dkcrlPWs-ciVl/pub?output=csv'
@@ -223,13 +225,14 @@ stock_weather_df = pd.read_csv(url)
 #print(stock_weather_df)
 plt.figure(figsize=(10,10))
 sns.regplot(x=stock_weather_df["MaxTemp"], y=stock_weather_df["percent_change"], line_kws={"color":"g","alpha":0.5,"lw":2})
-plt.title("How Temperature and Nasdaq Stock Price Are Related", loc='center', fontweight='bold')
-plt.xlabel("Temperature (Celsius)")
-plt.ylabel("Nasdaq Price Change %")
+plt.title("How Temperature and Nasdaq \nStock Price Are Related", loc='center', fontweight='bold', size = 30)
+plt.xlabel("Temperature (Celsius)", fontweight = "bold", size = 18)
+plt.ylabel("Nasdaq Price Change %", fontweight = "bold", size = 18)
+plt.tick_params(axis='both', labelsize = 14)
 plt.show()
 
 
-# In[113]:
+# In[149]:
 
 
 url = f'https://docs.google.com/spreadsheets/d/e/2PACX-1vSpI-1mMu_veN7L4W3STjh1QFzXf_13jH6vm4jd9hzolr-pq8fxV0VfBYftNycRkQ_dkcrlPWs-ciVl/pub?output=csv'
@@ -239,9 +242,16 @@ stock_weather_df = pd.read_csv(url)
 plt.figure(figsize=(10,10))
 sns.set(style="darkgrid")
 sns.boxplot(x=stock_weather_df["WeatherState"], y=stock_weather_df["percent_change"])
-plt.title("How Types of Weather and Stock Price Are Related", loc='center', fontweight='bold')
+plt.title("How Types of Weather and \nStock Price Are Related", loc='center', fontweight='bold', size = 30)
 plt.xticks(rotation = 45)
-plt.xlabel("Type of Weather")
-plt.ylabel("Nasdaq Stock Price Change %")
+plt.tick_params(axis='both', labelsize = 14)
+plt.xlabel("Type of Weather", fontweight = "bold", size = 18)
+plt.ylabel("Nasdaq Stock Price Change %", fontweight = "bold", size = 18)
 plt.show()
+
+
+# In[ ]:
+
+
+
 
